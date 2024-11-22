0c27132a5b8af39388f7e14758358a2838a48437
Divs-Git <divyansh.sri258@gmail.com>
2024-11-22 20:14:56 +0530

    added route and axios

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import "./App.css";
-
+import { Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import ChatPage from "./pages/ChatPage";
 function App() {
-  return <React.Fragment></React.Fragment>;
+  return (
+    <React.Fragment>
+      <Route path="/" component={HomePage} exact />
+      <Route path="/chats" component={ChatPage} />
+    </React.Fragment>
+  );
 }
 
 export default App;
